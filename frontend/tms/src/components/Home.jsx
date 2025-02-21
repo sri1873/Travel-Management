ce397c002596a9420c8cc32fb6123e74c73f9312
Olan Healy <olanhealy12@gmail.com>
2025-02-21 17:07:24 +0000

    Home page link to registraton and placeholder for login ahead, more consistent styling too

--- frontend/tms/src/components/Home.jsx
+++ frontend/tms/src/components/Home.jsx
@@ -1,8 +1,29 @@
-// src/components/Home.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
 
 const Home = () => {
-    return <h1>Welcome to the Home Page!</h1>;
+  return (
+    <div className="home-container">
+      <div className="logo-container">
+        <img
+          src="/tms_logo.png" 
+          alt="Travel Management System Logo"
+          className="logo"
+        />
+      </div>
+      <h1>Travel Management System</h1>
+      <p>Discover with ease</p>
+      <div className="buttons-container">
+        <Link to="/register">
+          <button className="home-button">Register</button>
+        </Link>
+        <Link to="/login">
+          <button className="home-button">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
