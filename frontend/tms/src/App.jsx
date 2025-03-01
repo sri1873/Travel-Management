c00ac3cb7d9d3e9a5d2860094e193fe7f5d1c373
Sri Kumar <kssrikumar180703@gmail.com>
2025-03-01 17:14:18 +0000

    added navbar and file refactors

--- frontend/tms/src/App.jsx
+++ frontend/tms/src/App.jsx
@@ -3,17 +3,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/Registration/RegistrationForm';
 import Home from './components/Home';  // Your home component or just a placeholder
 import FlightHome from './components/Flights/FlightHome';
+import Navbar from './components/Navbar';
+import './App.css'
+import Onboarding from './components/Registration/Onboarding';
 
 const App = () => {
     return (
+        <>
+        <Navbar/>
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />  {/*Add this route*/}
+                <Route path="/" element={<Onboarding />} />  {/*Add this route*/}
+                <Route path="/home" element={<Home />} />  {/*Add this route*/}
                 <Route path="/login" element={<FlightHome />} />  {/* Add this route */}
+                <Route path="/flights" element={<FlightHome />} />  {/* Add this route */}
                 <Route path="/register" element={<RegistrationForm />} />
                 {/* Add more routes as needed */}
             </Routes>
         </Router>
+        </>
     );
 };
 
