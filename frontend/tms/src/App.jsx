4171fb682817fa22834546d873e17c58e2ac2960
Sri Kumar <kssrikumar180703@gmail.com>
2025-03-01 16:31:28 +0000

    added flight home page

--- frontend/tms/src/App.jsx
+++ frontend/tms/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/Registration/RegistrationForm';
 import Home from './components/Home';  // Your home component or just a placeholder
+import FlightHome from './components/Flights/FlightHome';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />  {/* Add this route */}
+                <Route path="/" element={<Home />} />  {/*Add this route*/}
+                <Route path="/login" element={<FlightHome />} />  {/* Add this route */}
                 <Route path="/register" element={<RegistrationForm />} />
                 {/* Add more routes as needed */}
             </Routes>
