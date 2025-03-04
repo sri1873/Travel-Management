653d8e5abc9638ed10c3b6b87500d17d05887abc
Olan Healy <olanhealy12@gmail.com>
2025-03-04 16:22:15 +0000

    forgot password done

--- frontend/tms/src/components/Navbar.jsx
+++ frontend/tms/src/components/Navbar.jsx
@@ -139,88 +139,98 @@ const NavBar = () => {
       );
   } else {
     //User navbar
-    return (
-      <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-        <div className="container-fluid">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle d-flex align-items-center"
-                href="#"
-                id="brandDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <img
-                  src="public/icon.png"
-                  alt="Logo"
-                  width="50"
-                  height="40"
-                  className="d-inline-block align-text-top"
-                />
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="brandDropdown">
-                <li>
-                  <button className="dropdown-item" onClick={onLogoutClick}>
-                    Logout
-                  </button>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/change-password">
-                    Change Password
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <button
-            className="navbar-toggler"
-            style={{ backgroundColor: 'aliceblue' }}
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarScroll"
-            aria-controls="navbarScroll"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarScroll">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
-                  Dashboard
+    const handleForgotPassword = () => {
+    // For now, simulate a reset password action with an alert.
+    window.alert("An email has been sent to reset your password.");
+  };
+
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle d-flex align-items-center"
+              href="#"
+              id="brandDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src="public/icon.png"
+                alt="Logo"
+                width="50"
+                height="40"
+                className="d-inline-block align-text-top"
+              />
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="brandDropdown">
+              <li>
+                <button className="dropdown-item" onClick={onLogoutClick}>
+                  Logout
+                </button>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <Link className="dropdown-item" to="/change-password">
+                  Change Password
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/hotel">
-                  Hotels
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/flights">
-                  Flights
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/restaurants">
-                  Restaurants
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/trip">
-                  Plan a trip
-                </Link>
+              <li>
+                <button className="dropdown-item" onClick={handleForgotPassword}>
+                  Forgot Password
+                </button>
               </li>
             </ul>
-          </div>
+          </li>
+        </ul>
+        <button
+          className="navbar-toggler"
+          style={{ backgroundColor: 'aliceblue' }}
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarScroll">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/hotel">
+                Hotels
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/flights">
+                Flights
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/restaurants">
+                Restaurants
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/trip">
+                Plan a trip
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-};
+      </div>
+    </nav>
+  );
+}
+}
 export default NavBar;
