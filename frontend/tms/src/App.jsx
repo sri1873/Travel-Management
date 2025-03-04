fd2422b65ca3f14e16847948f880e8ac8ec8b8d5
Olan Healy <olanhealy12@gmail.com>
2025-03-04 18:00:46 +0000

    cleanup of files

--- frontend/tms/src/App.jsx
+++ frontend/tms/src/App.jsx
@@ -2,17 +2,16 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setCredentials } from './store/userSlice';
-import RegistrationForm from './components/Registration/RegistrationForm';
-import Home from './components/Home';
+import RegistrationForm from './components/UserManagement/RegistrationForm';
 import FlightHome from './components/Flights/FlightHome';
-import Navbar from './components/Navbar';
-import LoginForm from './components/Login/LoginForm';
-import Onboarding from './components/Registration/Onboarding';
-import VerifyEmail from './components/VerifyEmail/VerifyEmail';
+import Navbar from './components/Navigation/Navbar';
+import LoginForm from './components/UserManagement/LoginForm';
+import Onboarding from './components/OnBoarding/Onboarding';
+import VerifyEmail from './components/UserManagement/VerifyEmail';
 import DashboardPage from './components/Dashboard/DashboardPage';
 import AdminDashboard from './components/Dashboard/AdminDashboard';
 import ProtectedRoute from './components/ProtectedRoute';
-import ChangePasswordForm from './components/Password/ChangePasswordForm';
+import ChangePasswordForm from './components/UserManagement/ChangePasswordForm';
 import TravelAdminDashboard from './components/Dashboard/TravelAdminDashboard';
 import './App.css';
 
@@ -32,7 +31,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<ProtectedRoute />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/flights" element={<FlightHome />} />
         <Route path="/register" element={<RegistrationForm />} />
