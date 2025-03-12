f1df6e7fe81b4de7e837adf125712662249717e2
Sri Kumar <kssrikumar180703@gmail.com>
2025-03-12 12:06:22 +0000

    refactor

--- frontend/tms/src/App.jsx
+++ frontend/tms/src/App.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, HashRouter as Router, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import { setCredentials } from './store/userSlice';
-import RegistrationForm from './components/UserManagement/RegistrationForm';
-import FlightHome from './components/Flights/FlightHome';
-import Navbar from './components/Navigation/Navbar';
-import LoginForm from './components/UserManagement/LoginForm';
-import Onboarding from './components/OnBoarding/Onboarding';
-import VerifyEmail from './components/UserManagement/VerifyEmail';
-import DashboardPage from './components/Dashboard/DashboardPage';
-import AdminDashboard from './components/Dashboard/AdminDashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-import ChangePasswordForm from './components/UserManagement/ChangePasswordForm';
-import TravelAdminDashboard from './components/Dashboard/TravelAdminDashboard';
-import RestaurantList from './components/Restaurant/RestaurantList';
-import RestaurantDetail from './components/Restaurant/RestaurantDetail';
-import HotelDiscovery from './components/Hotels/HotelDiscovery/HotelDiscovery';
-import HotelDetails from './components/Hotels/HotelDetails/HotelDetails';
-import HotelBooking from './components/Hotels/HotelBooking/HotelBooking';
 import './App.css';
-import FlightSearch from './components/Flights/FlightSearch';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import DashboardPage from './components/Dashboard/DashboardPage';
+import TravelAdminDashboard from './components/Dashboard/TravelAdminDashboard';
 import FlightBooking from './components/Flights/FlightBooking';
 import FlightDetails from './components/Flights/FlightDetails';
 import FlightHome from './components/Flights/FlightHome';
+import FlightSearch from './components/Flights/FlightSearch';
+import HotelBooking from './components/Hotels/HotelBooking/HotelBooking';
+import HotelDetails from './components/Hotels/HotelDetails/HotelDetails';
+import HotelDiscovery from './components/Hotels/HotelDiscovery/HotelDiscovery';
+import Navbar from './components/Navigation/Navbar';
+import Onboarding from './components/OnBoarding/Onboarding';
+import RestaurantDetail from './components/Restaurant/RestaurantDetail';
+import RestaurantList from './components/Restaurant/RestaurantList';
+import ChangePasswordForm from './components/UserManagement/ChangePasswordForm';
+import LoginForm from './components/UserManagement/LoginForm';
+import RegistrationForm from './components/UserManagement/RegistrationForm';
+import VerifyEmail from './components/UserManagement/VerifyEmail';
 
 const App = () => {
     const dispatch = useDispatch();
@@ -55,8 +51,8 @@ const App = () => {
                 <Route path="/restaurants" element={<RestaurantList />} />
                 <Route path="/restaurants/:id" element={<RestaurantDetail />} />
                 <Route path="/hotels" element={<HotelDiscovery />} />
-                <Route path="/hotels/:id" element={<HotelDetails/>} />
-                <Route path="/booking/:hotelId/:roomId" element={<HotelBooking/>} />
+                <Route path="/hotels/:id" element={<HotelDetails />} />
+                <Route path="/booking/:hotelId/:roomId" element={<HotelBooking />} />
             </Routes>
         </Router>
     );
