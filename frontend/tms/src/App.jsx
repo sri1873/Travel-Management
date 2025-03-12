f29536542f5c0dcae6ecd54401ac9b55c4741289
suppixie <mamthapatalay6@gmail.com>
2025-03-12 13:02:56 +0000

    created booking history page

--- frontend/tms/src/App.jsx
+++ frontend/tms/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/Registration/RegistrationForm';
 import Home from './components/Home'; 
 import Itinerary from './components/itinerary/trip_packages';
+import BookingHistory from './components/itinerary/booking_history';
 
 const App = () => {
     return (
@@ -11,6 +12,9 @@ const App = () => {
                 <Route path="/" element={<Home />} />  {/* Add this route */}
                 <Route path="/register" element={<RegistrationForm />} />
                 <Route path="/itinerary" element={<Itinerary/>}/>
+                {/* <Route path="/activities" element={<BookingHistory/>}/> */}
+                <Route path="/bookinghistory" element={<BookingHistory/>}/>
+
             </Routes>
         </Router>
     );
