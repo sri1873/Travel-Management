f05cf45e7d49b6a708c394923609f54ad072e925
suppixie <mamthapatalay6@gmail.com>
2025-03-15 13:23:53 +0000

    added bookinghistory reducer

--- frontend/tms/src/store/store.js
+++ frontend/tms/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import flightReducer from "./flightSlice";
 import selectedFlightReducer from "./selectedFlightSlice";
+import bookingReducer from "./itineraryBookingSlicer";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     flight: flightReducer,
-    selectedFlight: selectedFlightReducer
+    selectedFlight: selectedFlightReducer,
+    bookingHistory: bookingReducer
   },
 });
